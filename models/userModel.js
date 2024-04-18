370fc18ab8b52a3bcc8cc4c498a782f3b6f399cd
Khoula-Khalid <143867345+Khoula-Khalid@users.noreply.github.com>
2024-04-19 07:35:30 +0800

    Add files via upload

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    is_online:{
+        type:String,
+        default:'0'
+    },
+    reset_password_token: {
+        type: String
+    },
+    resetPasswordTokenExpires: {
+        type: Date
+    }
+},
+{ timestamps:true }
+);
+
+module.exports = mongoose.model('User',userSchema);
